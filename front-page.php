8c9c9e3df2b77cca08652b8cc93aec522d84b19e
lancelynyrd <pinedaclp@gmail.com>
2016-03-31 17:20:21 +0800

    id error

--- front-page.php
+++ front-page.php
@@ -5,27 +5,27 @@ include 'part/front-info.php';
 ?>
     <section class="desc">
         <div>
-            <img src="<?php id() ?>/home_02.png" >
+            <img src="<?php id() ?>home_02.png" >
         </div>
     </section>
     <section class="gallery">
         <div>
-            <img src="<?php id() ?>/home_03.png" >
+            <img src="<?php id() ?>home_03.png" >
         </div>
     </section>
     <section class="book">
         <div>
-            <img src="<?php id() ?>/home_04.png" >
+            <img src="<?php id() ?>home_04.png" >
         </div>
     </section>
     <section class="book1">
         <div>
-            <img src="<?php id() ?>/home_05.png" >
+            <img src="<?php id() ?>home_05.png" >
         </div>
     </section>
     <section class="testimonial">
         <div>
-            <img src="<?php id() ?>/home_06.png" >
+            <img src="<?php id() ?>home_06.png" >
         </div>
     </section>
 <?php
--- part/front-info.php
+++ part/front-info.php
@@ -3,6 +3,6 @@ wp_enqueue_style('info', td() . '/css/front-info.css');
 ?>
 <section class="info container">
     <div class="row">
-        <img src="<?php id() ?>/home_01.png" >
+        <img src="<?php id() ?>home_01.png" >
     </div>
 </section>
